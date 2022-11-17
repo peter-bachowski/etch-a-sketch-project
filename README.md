00989dec0662b8c74e893f90fc2a256f3745dfb6
Peter <bachowskip@gmail.com>
2022-11-16 22:49:17 -0800

    Add a reset button, and add more code to js file for creating the grid. Still need grid to repeat line by line.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+This project will be a version of a very simple sketch pad, or Etch-a-Sketch, that is one of the assignments from The Odin Project. 
 
+This project is designed to showcase my ability to manipulate the DOM, as well as implement and create elements using javascript, and using listeners for the mouse drag event to change the color of div boxes and leave a trail behind, just like a sketch pad.
--- script.js
+++ script.js
@@ -1,8 +1,47 @@
 
 const createGridButton = document.getElementById("makeGrid"),
   inputText = document.getElementById("inputText"),
-  title = document.getElementById("header");
+  title = document.getElementById("header"),
+  gridContainer = document.getElementById("gridContainer"),
+  clearButton = document.getElementById("clearButton");
+
+let dimensions = 0;//the dimensions variable will be the dimension of your grid (how many boxes wide and high)
 
 createGridButton.onclick = function() {
-  title.style.color = "red";
+  clearGrid();
+  dimensions = inputText.value;//sets the dimensions varuable to whatever number is typed into the text box
+  if(isNaN(dimensions)){//if the value entered is not a number, throw an error
+    alert("Error! Please enter a number.");
+    inputText.value = null;
+  }
+  else if(dimensions < 0){//if the value entered is negative, throw an error
+    alert("Error! Please enter a non-negative value.");
+    inputText.value = null;
+  }
+  else{//otherwise, creates a new div box with the given id, then adds it to the grid container
+
+    for(x = 0; x < dimensions; x++){
+      gridContainer.innerHTML += "<br>";
+      for(y = 0; y < dimensions; y++){
+        createBox();
+      }
+    } 
+  }
+}
+
+clearButton.onclick = function() { //this clear button clears the sketch grid
+  clearGrid();
+}
+
+function clearGrid() {
+  while (gridContainer.firstChild){
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+}
+
+function createBox(){
+  const gridElement = document.createElement("div"); 
+  gridElement.id = "gridElement";
+  //gridElement.classList.add("mainGrid");
+  gridContainer.appendChild(gridElement);
 }
