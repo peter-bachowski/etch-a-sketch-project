2a3eac78edb56c2a5be9952d709c29e7436b561a
Replit user <>
2022-11-17 01:30:37 +0000

    Remove window.onload from script to allow button to work

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-window.onload{
-  const createGridButton = document.getElementById("makeGrid"),
-    inputText = document.getElementsByClassName("inputText");
 
-  createGridButton.addEventListener("click", () => {
-    div.style.color = "red";
-  });
+const createGridButton = document.getElementById("makeGrid"),
+  inputText = document.getElementById("inputText"),
+  title = document.getElementById("header");
 
+createGridButton.onclick = function() {
+  title.style.color = "red";
 }
